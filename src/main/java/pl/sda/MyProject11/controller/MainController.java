4549a53e264b97d2227078468f592e269752f9cf
rozan <rozan69@wp.pl>
2020-10-17 14:31:10 +0200

    Zmiany 2

--- src/main/java/pl/sda/MyProject11/controller/MainController.java
+++ src/main/java/pl/sda/MyProject11/controller/MainController.java
@@ -2,12 +2,11 @@ package pl.sda.MyProject11.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/index")
+ //   @RequestMapping("/index")
     public String index(Model model) {
 
 
